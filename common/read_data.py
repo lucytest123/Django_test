2cf4a219e7508b668e6e6d73f020bab9d1bead78
lucytest123 <1258881069@qq.com>
2023-02-17 16:46:20 +0800

    新增xls 文件读取方法

--- common/read_data.py
+++ common/read_data.py
@@ -3,67 +3,68 @@ import json
 import os.path
 
 import yaml
-import  xlrd
-from  Django_test.common.loggin import logger
+import xlrd
+from Django_test.common.loggin import logger
+
 
 class MyConfigParser(configparser):
-    def __int__(self,dafaults = None):
-        configparser.ConfigParser.__init__(self,defaults=dafaults)
-    def optionxform(self,optionstr):
+    def __int__(self, dafaults=None):
+        configparser.ConfigParser.__init__(self, defaults=dafaults)
+
+    def optionxform(self, optionstr):
         return optionstr
 
 
 def load_json(file_path):
     logger.info("加载{}文件.....".format(file_path))
-    with open(file_path)as f:
+    with open(file_path) as f:
         date = json.load(file_path)
     logger.info("读取数据内容：{}".format(date))
     return date
 
 
-class  readFileData():
+class readFileData():
     def __int__(self):
         pass
+
     # 读取yaml
-    def load_yaml(self,file_path):
+    def load_yaml(self, file_path):
         logger.info("加载{}文件....".format(file_path))
-        with open(file_path,encoding="utf-8")as  f:
+        with open(file_path, encoding="utf-8") as f:
             data = yaml.safe_load(f)
         logger.info("读取数据==》 {}".format(data))
         return data
 
-
-# 读取ini配置文件
-    def load_ini(self,file_path):
+    # 读取ini配置文件
+    def load_ini(self, file_path):
         logger.info("加载{}文件....".format(file_path))
         config = MyConfigParser()
-        config.read(file_path,encoding = "utf-8")
+        config.read(file_path, encoding="utf-8")
         data = dict(config._sections)
         logger.info("读取数据内容：{}..".format(data))
         return data
 
-
     # 读取xls文件 输出行数据
-    def load_xls_norws(self,file_path):
+    def load_xls_norws(self, file_path):
         if os.path.exists(file_path):
             logger.info("加载{}文件......".format(file_path))
             config = xlrd.open_workbook(filename=file_path)
             sheet = config.sheet_by_index(0)
-            nrows = sheet.nrows  #获取行数
-            logger.info("{}文件行数为：{}".format(file_path,nrows))
-            ncols = sheet.ncols  #获取列数
-            logger.info("{}文件列数为：{}".format(file_path,ncols))
-            for i in range(0,nrows):
+            nrows = sheet.nrows  # 获取行数
+            logger.info("{}文件行数为：{}".format(file_path, nrows))
+            ncols = sheet.ncols  # 获取列数
+            logger.info("{}文件列数为：{}".format(file_path, ncols))
+            for i in range(0, nrows):
                 books = sheet.row_values(i)
                 return books
 
         else:
             logger.debug("{}文件不存在".format(file_path))
-            return  FileNotFoundError
-
+            return FileNotFoundError
 
         # 读取xls文件中的列
-    def load_xls_ncols(self,file_path):
+
+    def load_xls_ncols(self, file_path):
         if os.path.exists(file_path):
             logger.info("加载{}文件......".format(file_path))
             config = xlrd.open_workbook(filename=file_path)
@@ -77,5 +78,7 @@ class  readFileData():
                 return books
         else:
             logger.debug("{}文件不存在".format(file_path))
-            return  FileNotFoundError
+            return FileNotFoundError
+
+
 data = readFileData()
