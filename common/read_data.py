b7b668ccf037a93fcce016b38d7dd37740687fba
lucytest123 <1258881069@qq.com>
2023-02-17 16:43:33 +0800

    新增xls 文件读取方法

--- common/read_data.py
+++ common/read_data.py
@@ -1,7 +1,9 @@
 import configparser as configparser
 import json
-import yaml
+import os.path
 
+import yaml
+import  xlrd
 from  Django_test.common.loggin import logger
 
 class MyConfigParser(configparser):
@@ -22,6 +24,7 @@ def load_json(file_path):
 class  readFileData():
     def __int__(self):
         pass
+    # 读取yaml
     def load_yaml(self,file_path):
         logger.info("加载{}文件....".format(file_path))
         with open(file_path,encoding="utf-8")as  f:
@@ -29,6 +32,8 @@ class  readFileData():
         logger.info("读取数据==》 {}".format(data))
         return data
 
+
+# 读取ini配置文件
     def load_ini(self,file_path):
         logger.info("加载{}文件....".format(file_path))
         config = MyConfigParser()
@@ -38,4 +43,39 @@ class  readFileData():
         return data
 
 
+    # 读取xls文件 输出行数据
+    def load_xls_norws(self,file_path):
+        if os.path.exists(file_path):
+            logger.info("加载{}文件......".format(file_path))
+            config = xlrd.open_workbook(filename=file_path)
+            sheet = config.sheet_by_index(0)
+            nrows = sheet.nrows  #获取行数
+            logger.info("{}文件行数为：{}".format(file_path,nrows))
+            ncols = sheet.ncols  #获取列数
+            logger.info("{}文件列数为：{}".format(file_path,ncols))
+            for i in range(0,nrows):
+                books = sheet.row_values(i)
+                return books
+
+        else:
+            logger.debug("{}文件不存在".format(file_path))
+            return  FileNotFoundError
+
+
+        # 读取xls文件中的列
+    def load_xls_ncols(self,file_path):
+        if os.path.exists(file_path):
+            logger.info("加载{}文件......".format(file_path))
+            config = xlrd.open_workbook(filename=file_path)
+            sheet = config.sheet_by_index(0)
+            nrows = sheet.nrows  # 获取行数
+            logger.info("{}文件行数为：{}".format(file_path, nrows))
+            ncols = sheet.ncols  # 获取列数
+            logger.info("{}文件列数为：{}".format(file_path, ncols))
+            for i in range(0, ncols):
+                books = sheet.col_slice(i)
+                return books
+        else:
+            logger.debug("{}文件不存在".format(file_path))
+            return  FileNotFoundError
 data = readFileData()
