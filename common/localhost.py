83a58834d9dec6f158489d8b732b93342d045d1e
lucytest123 <93259096+lucytest123@users.noreply.github.com>
2023-06-02 11:53:30 +0800

    增加邮件异常判断

--- common/localhost.py
+++ common/localhost.py
@@ -3,15 +3,9 @@ from email.mime.text import MIMEText
 from email.header import Header
 from Django_test.common.loggin import logger
 
-"""
-邮件通知
- from_mail  : 邮件收件人
- to_mail_list ：邮件发送人
- note_content : 文件内容"""
-
-mail_host = ""  # 服务器地址
-mail_user = ""  # 用户名
-mail_pass = ""  # 口令
+mail_host = "smtp.qq.com"  # 服务器地址
+mail_user = "1258881069@qq.com"  # 用户名
+mail_pass = "123123"  # 口令
 
 
 def send_mails_lavalhost(receivers=[mail_user], subject="", content=""):
@@ -21,14 +15,17 @@ def send_mails_lavalhost(receivers=[mail_user], subject="", content=""):
     subject		str		发送的邮件主题
     content		str		发送的邮件内容
     """
+    if receivers is None:
+        receivers = [mail_user]
     msg = MIMEText(content, "plain", "utf-8")
-    msg["From"] = MIMEText("complate", "utf-8")
-    msg["To"] = MIMEText("test", "utf-8")
+    msg["From"] = Header("complate", "utf-8")
+    msg["To"] = Header("test", "utf-8")
+    msg["Subject"] = Header(subject, "utf-8")
     try:
         smtObj = smtplib.SMTP
-        smtObj.connect(mail_host, 25)
+        smtObj.connect(mail_host, 25)  # 25 为smtp服务的端口号
         smtObj.login(mail_user, mail_pass)
-        smtObj.sendmail(receivers, subject, msg.as_string())
+        smtObj.sendmail(mail_user, receivers, msg.as_string())
         logger.info("邮件发送成功，收件人：｛｝".format(receivers))
     except smtplib.SMTPException:
         logger.debug("邮件发送失败，失败原因｛｝".format(smtplib.SMTPException))
