61db32e2da619fbdcd2748706abad0815027197e
lucytest123 <93259096+lucytest123@users.noreply.github.com>
2023-06-07 15:38:20 +0800

    增加视频获取总时长

---
+++ shipinweb/com/request.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+from Django_test.common.loggin import logger
+
+
+class video(object):
+    def __init__(self):
+        self.outtimes = 0
+        self.i = 0
+
+    def duration(self, url):
+        """获取视频总时间
+        url: 访问地址"""
+        response = requests.get(url)
+        sopu = BeautifulSoup(response.text, "html.parser")
+        video_duration = sopu.find("span", {"id": "video-time"})["date-content"]
+        logger.info("获取视频总时长为｛｝".format(video_duration))
+        return video_duration
